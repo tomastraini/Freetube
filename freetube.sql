af1ade06dc14aceb4b46858acd69f9e103350146
tomastraini <93160827+tomastraini@users.noreply.github.com>
2021-10-25 17:51:11 -0300

    Add files via upload

---
+++ freetube.sql
@@ -0,0 +1,161 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3307
+-- Tiempo de generación: 25-10-2021 a las 22:50:59
+-- Versión del servidor: 10.4.20-MariaDB
+-- Versión de PHP: 8.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `freetube`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comentarios`
+--
+
+CREATE TABLE `comentarios` (
+  `id_comentario` int(11) NOT NULL,
+  `comentario` varchar(600) NOT NULL,
+  `id_usuario` varchar(60) NOT NULL,
+  `id` int(11) NOT NULL,
+  `fecha` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `comentarios`
+--
+
+INSERT INTO `comentarios` (`id_comentario`, `comentario`, `id_usuario`, `id`, `fecha`) VALUES
+(17, 'Que buen tema!', 'admin', 38, ''),
+(18, 'Jajajaj', 'admin', 21, ''),
+(19, 'Tr3mendo', 'ale76', 38, '');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` varchar(60) NOT NULL,
+  `nombreus` varchar(60) NOT NULL,
+  `apellidous` varchar(60) NOT NULL,
+  `contrasena` varchar(600) NOT NULL,
+  `correo` varchar(60) NOT NULL,
+  `permiso` varchar(60) NOT NULL,
+  `foto_perfil` varchar(600) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `nombreus`, `apellidous`, `contrasena`, `correo`, `permiso`, `foto_perfil`) VALUES
+('admin', 'Tomás', 'Traini', '3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2', 'locoscrafteros96@gmail.com', 'admin', 'default.png'),
+('ale76', 'Alejandra', 'García', '3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2', 'ma_lejandra30@gmail.com', 'user', 'flor logo 1.jpg'),
+('frank', 'Franco', 'Perezlindo', '3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2', 'franco.perezlindo@institutozonaoeste.edu.ar', 'user', 'default.png'),
+('martu', 'martu', 'cordoba', '3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2', 'martu1685@gmail.com', 'user', 'default.png');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `videos`
+--
+
+CREATE TABLE `videos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(600) NOT NULL,
+  `ubicacion` varchar(60) NOT NULL,
+  `id_usuario` varchar(60) NOT NULL,
+  `descripción` varchar(600) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `videos`
+--
+
+INSERT INTO `videos` (`id`, `nombre`, `ubicacion`, `id_usuario`, `descripción`) VALUES
+(21, 'Teo con 13', '-3-facebook-google-chrome-2021-07-19-19-28-24-437.mp4', 'admin', ''),
+(30, 'Mostrando', 'desktop-2021-313.mp4', 'admin', ''),
+(35, 'Corre!', 'x-851.mp4', 'admin', ''),
+(36, 'gg', 'xd2-122-585.mp4', 'martu', ''),
+(38, 'All along the watchtower cover', 'all-along-the-watchtower-720p-hd-325.mp4', 'ale76', ''),
+(40, 'Valorcio', 'vid-20211015-wa0008-25.mp4', 'admin', ''),
+(41, 'AxD', 'vid-20210625-wa0006-343.mp4', 'admin', '');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD PRIMARY KEY (`id_comentario`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id` (`id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- Indices de la tabla `videos`
+--
+ALTER TABLE `videos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  MODIFY `id_comentario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT de la tabla `videos`
+--
+ALTER TABLE `videos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD CONSTRAINT `comentarios_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`),
+  ADD CONSTRAINT `comentarios_ibfk_2` FOREIGN KEY (`id`) REFERENCES `videos` (`id`);
+
+--
+-- Filtros para la tabla `videos`
+--
+ALTER TABLE `videos`
+  ADD CONSTRAINT `videos_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
